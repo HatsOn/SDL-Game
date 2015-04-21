87a198c44e0d2012b5faf9e8452ea7a050e0b0fa
capitanliteral <anaraguen@gmail.com>
2015-04-21 20:01:16 +0100

    :bomb:  Prepare to die :bomb:

--- ModulePlayer.cpp
+++ ModulePlayer.cpp
@@ -184,9 +184,9 @@ update_status ModulePlayer::Update()
 	if (App->input->GetKey(SDL_SCANCODE_SPACE) == KEY_UP)
 	{
 		int delay = 100;
-		bombPosition.x = position.x;
-		bombPosition.y = position.y;
-		/*last_bomb = */App->particles->AddParticle(App->particles->bomb, bombPosition.x, bombPosition.y, COLLIDER_PLAYER_SHOT, 30);
+		bombPosition.x = 10;
+		bombPosition.y = 10;
+		/*last_bomb = */App->particles->AddParticle(App->particles->bomb, bombPosition.x, bombPosition.y, COLLIDER_PLAYER_SHOT);
 		LOG("bomba");
 	}
 
