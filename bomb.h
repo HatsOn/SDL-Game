f1ad23cdbdc1c4ebd3982c22da594181f66e1b23
CapitanLiteral <anaraguen@gmail.com>
2015-05-06 11:54:52 +0100

    :bomb:

--- bomb.h
+++ bomb.h
@@ -1,9 +1,20 @@
 #include "Module.h"
+#include "p2Point.h"
+#include "p2DynArray.h"
+#include "Module.h"
+#include "Animation.h"
+#include "Globals.h"
+#include "p2Point.h"
+#include "p2List.h"
+#include "ModuleCollision.h"
 
 class bomb : public Module
 {
 public:
 	int timer;
 	p2Point<int> p;
+	p2DynArray<Particle> particulas;
+	
+	bomb(Application* app, bool start_enabled);
 
 };
