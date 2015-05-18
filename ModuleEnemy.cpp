e43073e983ef09d39d0aefe48098b1ef8907e7c5
weprikjm <ruben.idigora@gmail.com>
2015-05-18 22:22:43 +0200

    ModuleEnemy i class enemy barebone

---
+++ ModuleEnemy.cpp
@@ -0,0 +1,26 @@
+#include "ModuleEnemy.h"
+#include "Application.h"
+
+bool ModuleEnemy::Start()
+{
+	
+
+
+
+
+}
+
+bool ModuleEnemy::CleanUp()
+{
+
+}
+
+void ModuleEnemy::OnCollision(Collider*, Collider*)
+{
+
+}
+
+void ModuleEnemy::UpdatePosition()
+{
+
+}
