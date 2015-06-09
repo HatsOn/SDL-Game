034e4bb95545cc8a6c30eab5ee523ad6bfa02986
CapitanLiteral <anaraguen@gmail.com>
2015-06-09 19:09:18 +0100

    Anant tirant amb el BOSS :+1:

---
+++ ModuleBoss.cpp
@@ -0,0 +1,52 @@
+#include "Globals.h"
+#include "Application.h"
+#include "ModuleBoss.h"
+
+ModuleBoss::ModuleBoss(Application* app, bool start_enabled) : Module(app, start_enabled)
+{
+	graphics = NULL;
+	current_animation = NULL;
+	speed.x = 1;
+	speed.y = 1;
+
+
+	// idle animation (just the bomberman
+	smashing.frames.PushBack({ 515, 213, 77, 100 });
+	// move upwards
+	smashing.frames.PushBack({ 515, 213, 77, 100 });
+	smashing.frames.PushBack({ 591, 213, 56, 100 });
+	smashing.frames.PushBack({ 658, 213, 56, 100 });
+	smashing.frames.PushBack({ 719, 249, 58, 116 });
+	smashing.loop = true;
+	smashing.speed = 0.5f;
+
+
+	current_animation = &idle;
+
+}
+
+
+ModuleBoss::~ModuleBoss()
+{
+}
+
+
+
+bool ModuleBoss::Start()
+{
+	App->particles->Enable();
+	App->collision->Enable();
+
+	LOG("Loading BOSS");
+
+	//caminar_fx = App->audio->LoadFx("caminar.ogg");
+
+	graphics = App->textures->Load("BombermanTiles.png");
+
+	speed.x = 0;
+	speed.y = 0;
+	position.x = 100;
+	position.y = 100;
+
+	return true;
+}
---
+++ ModuleBoss.h
@@ -0,0 +1,33 @@
+#pragma once
+#include "Module.h"
+#include "Animation.h"
+#include "Globals.h"
+#include "p2Point.h"
+#include "bomb.h"
+
+class ModuleBoss :
+	public Module
+{
+
+public:
+
+	SDL_Texture* graphics;
+	Collider* bossCollider;
+	Collider* mazeCollider;
+	Animation* current_animation;
+	Animation idle;
+	Animation smashing;
+	
+
+	p2Point<int> speed;
+	p2Point<int> position;
+
+
+
+	ModuleBoss(Application* app, bool start_enabled = true);
+
+	~ModuleBoss();
+
+	bool Start();
+};
+
