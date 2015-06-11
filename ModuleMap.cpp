335deead28d14e0a7df282669cfd124a3bc56634
capitanliteral <anaraguen@gmail.com>
2015-06-12 00:47:41 +0100

    tileboss

--- ModuleMap.cpp
+++ ModuleMap.cpp
@@ -56,7 +56,7 @@ update_status ModuleMap::Update()
 	else if (App->input->GetKey(SDL_SCANCODE_F9) == KEY_UP)
 	{
 		//App->tileMap->Enable();
-		App->fade->FadeToBlack(this, App->map, 3.0f);
+		App->fade->FadeToBlack(this, App->tileBoss, 3.0f);
 		App->audio->PlayMusic("BombermanStart.ogg", 0.5f);
 	}
 
